4b97530d3a7699011850b58e43c7d15d51d94c28
diogojpa99 <diogo.jpa.99@gmail.com>
2021-10-03 16:43:39 +0100

    Grafo done! Como fazemos a porcaria de um calendário???

--- readFile.c
+++ readFile.c
@@ -6,26 +6,25 @@
 // Implementar o grafo pelo método de lista de adjacências
 
 typedef struct _Adj{
-    char *id, *neighbor; //What's the commercial relation between me and my "list head"
-    int type, cost;   //what's the cost from the link between me and my "list head"
+    int id, neighbor, type, cost; //What's the commercial relation between me and my "list head"  and what's the cost from the link between me and my "list head"
     struct _Adj *next;
 } Adj;
 
 typedef struct _Nodes{
-    char *id;
+    int id;
     Adj *adjHead;
     struct _Nodes *next;
 } Nodes;
 
-Nodes *createNode(Nodes *listHead, char *tail, char *head, int type);
+Nodes *createNode(Nodes *listHead, int tail, int head, int type);
 
-Adj *createAdj(Adj *listHead, char *tail, char *head, int type);
+Adj *createAdj(Adj *listHead, int tail, int head, int type);
 
 Adj *insertAdj(Adj *listHead, Adj *newAdj);
 
-Nodes *insertNode(Nodes *listHead, Nodes *newNode);
+Nodes *insertNode(Nodes *listHead, Nodes *newNode, int id);
 
-Nodes *searchNodesList(Nodes *listHead, char *id);
+Nodes *searchNodesList(Nodes *listHead, int id);
 
 void Print_List_of_Adjacencies(Nodes *listHead);
 
@@ -34,8 +33,7 @@ void Print_List(Nodes *listHead);
 int main()
 {
     FILE *fp;
-    char tail[12], head[12];
-    int type;
+    int tail, head, type;
     Nodes *NodesHead = NULL, *newNode=NULL;
 
     fp = fopen("grafo1.txt","r");
@@ -45,25 +43,24 @@ int main()
         return(-1);
     }
 
-    while( fscanf(fp, "%s %s %d\n", tail, head, &type) != EOF ){
-        printf("%s %s %d\n", tail, head, type);
+    while( fscanf(fp, "%d %d %d\n", &tail, &head, &type) != EOF ){
         NodesHead = createNode(NodesHead, tail, head, type);
     }
 
-    Print_List(NodesHead);
-    //xPrint_List_of_Adjacencies(NodesHead);
+    //Print_List(NodesHead);
+    Print_List_of_Adjacencies(NodesHead);
 
     fclose(fp);
     return 0;
 }
 
 /** createNode: Creates a new node in the Nodes List **/
-Nodes *createNode(Nodes *listHead, char *tail, char *head, int type){ 
+Nodes *createNode(Nodes *listHead, int tail, int head, int type){ 
 
     Nodes *newNode=NULL;
     Adj *newAdj=NULL;
-
-    if (searchNodesList( listHead, tail) == NULL){ //Deve ser tail ou head ? Perguntar ao prof
+    
+    if (searchNodesList(listHead, tail) == NULL){ //Deve ser tail ou head ? Perguntar ao prof
         if((newNode = (Nodes*) malloc(sizeof(Nodes))) == NULL){   /** Creation of a New Node **/
             printf("Memory is full. Couldn't register request.\n");
 		    return listHead;
@@ -71,44 +68,31 @@ Nodes *createNode(Nodes *listHead, char *tail, char *head, int type){
         newNode->id=tail;
         newNode->next=NULL;
         newNode->adjHead=NULL;
-        printf("id=%s\n", newNode->id);      
-    } 
+        listHead=insertNode(listHead, newNode, newNode->id);    
+    }
     else{
         newNode = searchNodesList(listHead, tail);
-        printf("id=%s\n", newNode->id);         
     }
 
     newAdj= createAdj(newNode->adjHead, tail, head, type);
     newNode->adjHead=insertAdj(newNode->adjHead, newAdj);
-    listHead=insertNode(listHead, newNode);
-
-    //printf("New node: id=%s |-> Adj: id=%s| neighbor=%s| type=%d\n",newNode->id,newAdj->id, newAdj->neighbor, newAdj->type );
 
     return listHead;
 }
 
 /** Creates a new adjacent nodes referent to the Node in the Nodes List **/
-Adj *createAdj(Adj *listHead, char *tail, char *head, int type){ //Pensar sobre qual é que vai ser a listHead das Adj
+Adj *createAdj(Adj *listHead, int tail, int head, int type){ 
 
     Adj *newAdj;
 
     /** Creation of a New Node **/
     if((newAdj = (Adj*) malloc(sizeof(Adj))) == NULL){
-        printf("Memory is full. Couldn't register request.\n");
 		return listHead;
     }
 
     newAdj->neighbor=tail;
     newAdj->id=head;
-    if( type==2){
-        newAdj->type=2;
-    } 
-    else if( type==1){
-        newAdj->type=3;
-    } 
-    else if( type==3){
-        newAdj->type=1;
-    }
+    newAdj->type=type;
 
     return newAdj;
 }
@@ -133,27 +117,24 @@ Adj *insertAdj(Adj *listHead, Adj *newAdj){
     return listHead;
 }
 
-Nodes *insertNode(Nodes *listHead, Nodes *newNode){ //Insert the new Node in the end of the Nodes List
+Nodes *insertNode(Nodes *listHead, Nodes *newNode, int id){ //Insert the new Node in the end of the Nodes List
 
-    Nodes *auxH, *auxT;
+    Nodes *auxT;
 
     /** Inserting the new node in the end of the nodes list and returning the list head**/
     if(listHead==NULL){
         listHead=newNode;
     }else{
-        auxH=listHead;
-        auxT=listHead->next;
-        while(auxT != NULL){
-            auxH=auxT;
+        auxT=listHead;
+        while(auxT->next != NULL){
+            if( auxT->id == id){
+                return listHead;
+            }
             auxT=auxT->next;
-
-            if(strcmp(auxH->id,"17228")==0)
-                printf("auxT=%s ",auxT->id);
         }
-        auxH->next=newNode;
+        auxT->next=newNode;
         newNode->next=NULL;
     }
-    //Print_List(listHead);
     return listHead;
 }
 
@@ -162,26 +143,22 @@ Nodes *insertNode(Nodes *listHead, Nodes *newNode){ //Insert the new Node in the
 * If there is a match the function retunrs 1, theis means that the Node is already in the Nodes List, so we don´t create the        * 
 * same Nome two times. If there is no match, the fuction returns zero and we can create the new node and add him to the Nodes List. *
 *   */
-Nodes *searchNodesList(Nodes *listHead, char *id){ 
+Nodes *searchNodesList(Nodes *listHead, int id){ 
 
-    Nodes *auxH, *auxT;
+    Nodes *auxT;
 
-    auxH=listHead;
-
-    if(auxH == NULL){
+    if(listHead == NULL){
         return NULL;
     }else{     
-        auxT=listHead->next;
-        while(auxT !=NULL){
-            if( strcmp(auxH->id,id) == 0 ){
-                printf("%s==%s\n", auxH->id, id);
-                return auxH;
-            }
-            else{
-                return NULL;
-            }
-            auxH=auxT;
+        auxT=listHead;
+        if(auxT->id == id)
+            return auxT;
+        while(auxT->next !=NULL){
             auxT=auxT->next;
+            if( auxT->id == id ){
+                //printf("searchNodesList: auxT encontrado: %d\n", auxT->id); // DÚVIDA: A imprimir duas vezes, porquê ?
+                return auxT;
+            }
         }
     }
     return NULL;
@@ -189,28 +166,19 @@ Nodes *searchNodesList(Nodes *listHead, char *id){
 
 void Print_List_of_Adjacencies(Nodes *listHead){
     
-    Nodes *nodes_auxH, *nodes_auxT;
-    Adj *adj_auxH, *adj_auxT;
+    Nodes *nodes_auxT;
+    Adj *adj_auxT;
 
     if(listHead==NULL){
         return;
     }else{
-        nodes_auxH=listHead;
-        nodes_auxT=listHead->next;
-        while(nodes_auxT != NULL){
-            printf("[%s]->", nodes_auxH->id);
-            adj_auxH=nodes_auxH->adjHead;
-            adj_auxT=nodes_auxH->adjHead->next;
-            while( adj_auxT != NULL){
-                printf("[id:%s|type:%d|neighbor:%s]->", adj_auxH->id, adj_auxH->type, adj_auxH->neighbor);
-                adj_auxH=adj_auxT;
-                adj_auxT=adj_auxT->next;
+        for(nodes_auxT = listHead; nodes_auxT != NULL; nodes_auxT = nodes_auxT->next) {
+            printf("[%d]->", nodes_auxT->id);   fflush(stdout);
+            for(adj_auxT = nodes_auxT->adjHead; adj_auxT != NULL; adj_auxT = adj_auxT->next) {
+                printf("[id:%d|type:%d]->", adj_auxT->id, adj_auxT->type);  fflush(stdout);
             }
             printf("NULL\n");
-            nodes_auxH=nodes_auxT;
-            nodes_auxT=nodes_auxT->next;
         }
-        printf("NULL\n");
     }
 
     return;
@@ -221,17 +189,19 @@ void Print_List(Nodes *listHead){
     Nodes *auxH, *auxT;
     
     if(listHead==NULL){
+        printf("ups!\n");
         return;
     }else{
         auxH=listHead;
+        printf("id:%d\n",auxH->id);
         auxT=listHead->next;
         while( auxT != NULL){
-            printf("id:%s\n",auxH->id);
             auxH=auxT;
             auxT=auxT->next;
+            printf("id:%d\n",auxH->id);
         }
-    }
         printf("NULL\n");
+    }
 
     return;
-}
+} 
