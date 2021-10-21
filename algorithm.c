6449e8a2cef884f8c60c936623a5736790edad2f
diogojpa99 <diogo.jpa.99@gmail.com>
2021-10-21 18:01:00 +0100

    Antes de merdge

--- algorithm.c
+++ algorithm.c
@@ -30,30 +30,33 @@ void ReverseDijkstra(Nodes *nodes_head, Nodes *destiny_node){
 
     //Colocar o destino na pilha com custo zero 
     Q_1=InsertQ(destiny_node, Q_1);
-
-    while (Q_1 != NULL){
-        //retirar o nó que está na cabeça da pilhaS
-
-        if(Q_1->cost < Q_1->node->destHead->cost){ //Temos que atualizar -> Atualizamos só quando é melhor ou sempre ??
-            Q_1->node->destHead->cost=Q_1->cost;
-            //node->destHead->chosen_neighbour_id=??
-            
+    
+    while ( (Q_1 != NULL) && (Q_2 != NULL) && (Q_3 != NULL)){
+        while (Q_1 != NULL){
+            //retirar o nó que está na cabeça da pilhaS
             //Ver os meus vizinhos (Mas atenção às restrições comerciais)
+       
             //Enquanto a lista de adjacências não estiver vazia
+            //Relaxation of the link uv
+            //Ver os meus vizinhos (Mas atenção às restrições comerciais)
             //Vamos processar esse nó criar um elemento e inserir na respetiva pilha 
-
+            Q_1=Relaxation(Q_1, &Q_1, &Q_2, &Q_3);
             //Depois libertamos a cabeça da pilha atual e metemos a nova cabeça
+            Q_1=RemoveTopNodeFromQ(Q_1);
+
         }
 
+        while (Q_2 != NULL){
+            Q_2=Relaxation(Q_2, &Q_1, &Q_2, &Q_3);
+            Q_2=RemoveTopNodeFromQ(Q_2);
+        }
+
+        while (Q_3 != NULL){
+            Q_3=Relaxation(Q_3, &Q_1, &Q_2, &Q_3);  
+            Q_2=RemoveTopNodeFromQ(Q_2);
+        }
     }
 
-    while (Q_2 != NULL){
-        
-    }
-
-    while (Q_2 != NULL){
-        
-    }
     return;
 }
 
@@ -117,7 +120,75 @@ Queue *CreateNewElement(Queue *new_element, Nodes *node){
     return new_element;
 }
 
-Nodes *RemoveTopNodeFromQ(Queue *Q){
+Queue *RemoveTopNodeFromQ(Queue *Q){
 
+    Queue *auxH=NULL;
 
+    auxH=Q;
+    Q=Q->next;
+    free(auxH);
+
+    return Q;
+
+}
+
+//Relaxação do link uv
+Queue *Relaxation(Queue *Q, Queue *Q1, Queue *Q2, Queue *Q3){
+
+    Adj *neighbour=NULL;
+
+    if(Q == NULL){
+        return Q;
+    }else{
+        neighbour = Q->node->adjHead;
+        if( (Q->type <= 1 || neighbour->type <= 1) && (neighbour->id != Q->node->destHead->chosen_neighbour_id) ) //Acho que faz sentido, mas confirmar
+            Q = RelaxOfLink(Q, neighbour->node_pointer, neighbour->type, &Q1, &Q2, &Q3);
+        while(neighbour->next != NULL){
+            neighbour = neighbour->next;
+            if( (Q->type <= 1 || neighbour->type <= 1) && (neighbour->id != Q->node->destHead->chosen_neighbour_id))
+                Q = RelaxOfLink(Q, neighbour->node_pointer, neighbour->type, &Q1, &Q2, &Q3);
+        }
+    }
+    return Q;
+}
+
+Queue *RelaxOfLink(Queue *Q, Nodes *adj_node, int adj_node_type, Queue *Q1, Queue *Q2, Queue *Q3){
+
+    if( adj_node_type == 1){
+        adj_node_type = 3;
+    } else if (adj_node_type == 3){
+        adj_node_type = 1;
+    }
+
+    if( adj_node_type < adj_node->destHead->type){
+        adj_node->destHead->type=adj_node_type;
+        adj_node->destHead->cost=Q->cost;
+        adj_node->destHead->chosen_neighbour_id=Q->node_id;
+        ChooseQ( &Q1 , &Q2 , &Q3 , adj_node, adj_node->destHead->type);
+    } 
+    else if(adj_node_type == adj_node->destHead->type) {
+        if( (Q->cost+1) < adj_node->destHead->cost){
+            adj_node->destHead->type=adj_node_type;
+            adj_node->destHead->cost=Q->cost;
+            adj_node->destHead->chosen_neighbour_id=Q->node_id;
+            ChooseQ( &Q1 , &Q2 , &Q3 , adj_node, adj_node->destHead->type);
+        }
+    }
+
+    return Q;
+}
+
+void ChooseQ(Queue *Q1,Queue *Q2,Queue *Q3, Nodes *node, int type){
+
+    Queue *Q;
+
+    if( type == 1){
+        Q=InsertQ(node, Q1);
+    } else if (type ==2){
+        Q=InsertQ(node, Q2);
+    } else if (type == 3){
+        Q=InsertQ(node, Q3);
+    }
+
+    return;
 }
--- algorithm.h
+++ algorithm.h
@@ -13,4 +13,12 @@ Queue *InsertQ(Nodes *node, Queue *Q);
 
 Queue *CreateNewElement(Queue *new_element, Nodes *node);
 
+Queue *RemoveTopNodeFromQ(Queue *Q);
+
+Queue *Relaxation(Queue *Q, Queue *Q1, Queue *Q2, Queue *Q3);
+
+Queue *RelaxOfLink(Queue *Q, Nodes *adj_node, int adj_node_type, Queue *Q1, Queue *Q2, Queue *Q3);
+
+void ChooseQ(Queue *Q1,Queue *Q2,Queue *Q3, Nodes *node, int type);
+
 #endif //READFILE INCLUDED
