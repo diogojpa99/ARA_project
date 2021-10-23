db99119ac660b7f1399d773abfa9b3d56dcc3f3b
migasribas <miguelquinteiroribeiro@gmail.com>
2021-10-23 19:56:35 +0100

    Estatisticas feitas

--- CCDF_times_simulations.py
+++ CCDF_times_simulations.py
@@ -11,12 +11,11 @@ import pandas as pd
 ##Just Simulation
 data_times = np.loadtxt('times_simulations.txt')
 
+fig1, plot1 = plt.subplots()
 # getting data of the histogram
 count, bins_count = np.histogram(data_times, bins=10)
-plt.hist(data_times, bins_count, label='Histograma tos tempos de término das simulações')
-plt.legend()
+plot1.hist(data_times, bins_count, label='Histograma tos tempos de término das simulações')
 
-#plt.show()
 
 
 # finding the PDF of the histogram using count values
@@ -28,29 +27,12 @@ cdf = np.cumsum(pdf)
 ccdf = 1 - cdf 
 
 # plotting CDF and CCDF
-#plt.plot(bins_count[1:], pdf, color = "red", label = "PDF")
-plt.plot(bins_count[1:], cdf, color = 'green', label = "Times CDF")
-plt.plot(bins_count[1:], ccdf, color = "blue", label = "Times CCDF")
-plt.legend()
+fig2, plot2 = plt.subplots()
+plot2.plot(bins_count[1:], cdf, color = 'green', label = "Times CDF")
+plot2.plot(bins_count[1:], ccdf, color = "blue", label = "Times CCDF")
+plot2.legend()
+
 plt.show()
 
 
-                    ## TYPES AND COSTS OF ROUTES
-##Simulation
-data_types_costs = np.loadtxt('types_costs_simulations.txt')
-data_types = np.copy(data_types_costs[:,0])
-data_costs = np.copy(data_types_costs[:,1])
-
-#PDF of the type of route with which a node reaches a destination, over all nodes and all destinations
-# getting data of the histogram
-count, bins_count = np.histogram(data_types, bins=10)
-plt.hist(data_types, bins_count)
-plt.show()
-
-
-# finding the PDF of the histogram using count values
-pdf = count / sum(count)
-plt.plot(bins_count[1:], pdf, color = "red", label = "Types PDF")
-plt.legend()
-plt.show()
 
